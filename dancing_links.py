cb0a197fad0237a1ef9b072553baf442f8caf5fc
Will Church <willchurch1@gmail.com>
2017-04-06 20:04:12 -0500

    Add Cell.walk; Tidy up; Move build_partial_solution to DancingLinks

--- dancing_links.py
+++ dancing_links.py
@@ -17,20 +17,29 @@ class Cell(object):
         self.down = down or self
 
     def remove_horizontal(self):
-        self.right.left = self.left
-        self.left.right = self.right
+        self.right.left, self.left.right = self.left, self.right
 
     def restore_horizontal(self):
-        self.right.left = self
-        self.left.right = self
+        self.right.left, self.left.right = self, self
 
     def remove_vertical(self):
-        self.up.down = self.down
-        self.down.up = self.up
+        self.up.down, self.down.up = self.down, self.up
 
     def restore_vertical(self):
-        self.up.down = self
-        self.down.up = self
+        self.up.down, self.down.up = self, self
+
+    def walk(self, direction="right", inc=False):
+        """Iterate through all links in the direction specified by direction.
+        Valid directions are "left, "right", "up", and "down".
+        Set inc to True to include the object you are calling walk on.
+        """
+        if inc:
+            yield self
+        cell = getattr(self, direction)
+        while cell != self:
+            yield cell
+            cell = getattr(cell, direction)
+
 
 
 class Header(Cell):
@@ -64,47 +73,25 @@ class DancingLinks(Cell, metaclass=ABCMeta):
     def build_constraints(self):
         pass
 
-    @abstractmethod
-    def build_partial_solution(self, matrix):
-        pass
-
     def cover(self, column):
         # Unlink the column header
         column.remove_horizontal()
 
-        # Walk down the column
-        col_cell = column.down
-        while col_cell != column:
-
-            # Walk right across the row
-            row_cell = col_cell.right
-            while row_cell != col_cell:
-
+        for col_cell in column.walk('down'):
+            for row_cell in col_cell.walk('right'):
                 # Remove each cell in the row from its column
                 # and decrement its column's size
                 row_cell.remove_vertical()
                 row_cell.column.size -= 1
 
-                row_cell = row_cell.right
-
-            col_cell = col_cell.down
-
     def uncover(self, column):
-        # Walk up the column
-        col_cell = column.up
-        while col_cell != column:
 
-            # Walk left across the row
-            row_cell = col_cell.left
-            while row_cell != col_cell:
+        for col_cell in column.walk('up'):
+            for row_cell in col_cell.walk('left'):
 
                 # Restore each cell in the row and increment its column's size
                 row_cell.restore_vertical()
                 row_cell.column.size += 1
-
-                row_cell = row_cell.left
-
-            col_cell = col_cell.up
 
         # Restore the column header
         column.restore_horizontal()
@@ -139,14 +126,7 @@ class DancingLinks(Cell, metaclass=ABCMeta):
                 return
 
             # Find the column with the smallest number of cells
-            min_size = math.inf
-            smallest_col = None
-            column = self.right
-            while column != self:
-                if column.size < min_size:
-                    min_size = column.size
-                    smallest_col = column
-                column = column.right
+            smallest_col = min(self.walk(), key=lambda x: x.size)
 
             # Check if out of options
             if smallest_col.size < 1:
@@ -156,17 +136,14 @@ class DancingLinks(Cell, metaclass=ABCMeta):
             self.cover(smallest_col)
 
             # Walk down the column and try choosing every row
-            row_cell = smallest_col.down
-            while row_cell != smallest_col:
+            for row_cell in smallest_col.walk('down'):
 
                 # Add the chosen row to the solution set
                 solution_set.append(row_cell)
 
                 # Cover every column satisfied by the chosen row
-                col_cell = row_cell.right
-                while col_cell != row_cell:
+                for col_cell in row_cell.walk('right'):
                     self.cover(col_cell.column)
-                    col_cell = col_cell.right
 
                 # Continue the search recursively
                 search(depth + 1)
@@ -174,17 +151,41 @@ class DancingLinks(Cell, metaclass=ABCMeta):
                 # Undo the previous operation
                 row_cell = solution_set.pop()
 
-                col_cell = row_cell.left
-                while col_cell != row_cell:
+                for col_cell in row_cell.walk('left'):
                     self.uncover(col_cell.column)
-                    col_cell = col_cell.left
-
-                row_cell = row_cell.down
 
             self.uncover(smallest_col)
 
         search(0)
         print("Max depth: {}".format(self.max_depth))
+
+    def build_partial_solution(self, matrix):
+        assert len(matrix[0]) == self.width
+        assert len(matrix) == self.height
+
+        solution_set = []
+        for row_index, row, col_index, num in ((ri, r, ci, n) for ri, r in enumerate(matrix)
+                                                              for ci, n in enumerate(r)):
+            if num != 0:
+                # Find a cell that matches row_index col_index, num
+                # Dumb Search: This could probably be sped up by looking at the column names
+                name = "{0}:{1}:{2}".format(row_index, col_index, num)
+                cell = None
+                for column, row_cell in ((c, r) for c in self.walk()
+                                                for r in c.walk('down')):
+                    if row_cell.name == name:
+                        cell = row_cell
+                        break
+
+                # Add the cell to the solution set
+                assert cell is not None
+                solution_set.append(cell)
+
+                # Cover all satisfied columns
+                for row_cell in cell.walk('right', inc=True):
+                    self.cover(row_cell.column)
+
+        return solution_set
 
     def add_header(self, column):
         column.left = self.left
@@ -200,16 +201,11 @@ class DancingLinks(Cell, metaclass=ABCMeta):
 
     def print(self):
 
-        column = self.right
-        while column != self:
+        for column in self.walk():
             print("COLUMN: {}".format(column.name))
 
-            cell = column.down
-            while cell != column:
+            for cell in column.walk('down'):
                 print(cell.name)
-                cell = cell.down
-
-            column = column.right
 
 
 class LatinSquareDLX(DancingLinks):
@@ -238,14 +234,12 @@ class LatinSquareDLX(DancingLinks):
             new_header = Header(name=name_fmt.format(row, col, 'n'))
             self.add_header(new_header)
 
-        # Constraint 2: Each number from self.num_range
-        # must appear in every row in any column
+        # Constraint 2: Each number from self.num_range must appear in every row in any column
         for num, row in ((num, row) for num in self.num_range for row in range(self.height)):
             new_header = Header(name=name_fmt.format(row, 'c', num))
             self.add_header(new_header)
 
-        # Constraint 3: Each number from self.num_range
-        # must appear in every column in any row
+        # Constraint 3: Each number from self.num_range must appear in every column in any row
         for num, col in ((num, col) for num in self.num_range for col in range(self.width)):
             new_header = Header(name=name_fmt.format('r', col, num))
             self.add_header(new_header)
@@ -256,26 +250,22 @@ class LatinSquareDLX(DancingLinks):
                                               for num in self.num_range):
             prev = None
             name = name_fmt.format(row, col, num)
-            column = self.right
-            while column != self:
+
+            for column in self.walk():
                 col_row, col_col, col_num = column.name.split(':')
+
                 if (col_row == str(row) or col_row == 'r') and \
                    (col_col == str(col) or col_col == 'c') and \
                    (col_num == str(num) or col_num == 'n'):
+
                     cell = Cell(name=name)
                     column.add_cell(cell)
+
                     if prev is not None:
-                        cell.right = prev.right
-                        cell.left = prev
-                        prev.right.left = cell
-                        prev.right = cell
+                        # Add new cell to its row
+                        cell.right, cell.left = prev.right, prev
+                        prev.right.left, prev.right = cell, cell
                     prev = cell
-
-                column = column.right
-
-
-    def build_partial_solution(self, matrix):
-        pass
 
 
 class SudokuDLX(DancingLinks):
@@ -308,100 +298,62 @@ class SudokuDLX(DancingLinks):
             new_header = Header(name=rcn_fmt.format(row, col, 'n'))
             self.add_header(new_header)
 
-        # Constraint 2: Each number from num_range
-        # must appear in every row in any column
+        # Constraint 2: Each number from num_range must appear in every row in any column
         for num, row in ((num, row) for num in self.num_range for row in range(self.height)):
             new_header = Header(name=rcn_fmt.format(row, 'c', num))
             self.add_header(new_header)
 
-        # Constraint 3: Each number from self.num_range
-        # must appear in every column in any row
+        # Constraint 3: Each number from self.num_range must appear in every column in any row
         for num, col in ((num, col) for num in self.num_range for col in range(self.width)):
             new_header = Header(name=rcn_fmt.format('r', col, num))
             self.add_header(new_header)
 
-        # Constraint 4: Each number from self.num_range
-        # must appear in every zone
+        # Constraint 4: Each number from self.num_range must appear in every zone
         zn_format = "{0}:{1}"
         for zone, num in ((zone, num) for zone in range(9) for num in self.num_range):
             new_header = Header(name=zn_format.format(zone, num))
             self.add_header(new_header)
 
         # Next populate the columns
-        for row in range(self.height):
-            for col in range(self.width):
-                for num in self.num_range:
-                    prev = None
-                    name = rcn_fmt.format(row, col, num)
-                    column = self.right
-                    while column != self:
-                        column_name_items = column.name.split(':')
-                        if len(column_name_items) == 3:
-                            # "r:c:n" has 3 items
-                            col_row, col_col, col_num = column_name_items
-                            if (col_row == str(row) or col_row == 'r') and \
-                               (col_col == str(col) or col_col == 'c') and \
-                               (col_num == str(num) or col_num == 'n'):
-                                cell = Cell(name=name)
-                                column.add_cell(cell)
-                                if prev is not None:
-                                    cell.right = prev.right
-                                    cell.left = prev
-                                    prev.right.left = cell
-                                    prev.right = cell
-                                prev = cell
-                        else:
-                            # "z:n" has 2 items
-                            col_zone, col_num = column_name_items
-                            if get_zone(row, col) == col_zone and str(num) == col_num:
-                                cell = Cell(name=name)
-                                column.add_cell(cell)
-                                if prev is not None:
-                                    cell.right = prev.right
-                                    cell.left = prev
-                                    prev.right.left = cell
-                                    prev.right = cell
-                                prev = cell
+        for row, col, num in ((row, col, num) for row in range(self.height)
+                                              for col in range(self.width)
+                                              for num in self.num_range):
+            prev = None
+            name = rcn_fmt.format(row, col, num)
 
-                        column = column.right
+            for column in self.walk('right'):
 
+                column_name_items = column.name.split(':')
+                if len(column_name_items) == 3:
+                    # "r:c:n" has 3 items
 
-    def build_partial_solution(self, matrix):
-        assert len(matrix[0]) == self.width
-        assert len(matrix) == self.height
+                    col_row, col_col, col_num = column_name_items
+                    if (col_row == str(row) or col_row == 'r') and \
+                       (col_col == str(col) or col_col == 'c') and \
+                       (col_num == str(num) or col_num == 'n'):
 
-        solution_set = []
-        for row_index, row, col_index, num in ((ri, r, ci, n) for ri, r in enumerate(matrix)
-                                               for ci, n in enumerate(r)):
-            if num != 0:
-                # Find a cell that matches row_index col_index, num
-                # Dumb Search: This could probably be sped up by looking at the column names
-                name = "{0}:{1}:{2}".format(row_index, col_index, num)
-                cell = None
-                column = self.right
-                while column != self:
-                    row_cell = column.down
-                    while row_cell != column:
-                        if row_cell.name == name:
-                            cell = row_cell
-                            break
-                        row_cell = row_cell.down
-                    if cell is not None:
-                        break
-                    column = column.right
+                        cell = Cell(name=name)
+                        column.add_cell(cell)
 
-                # Add the cell to the solution set
-                assert cell is not None
-                solution_set.append(cell)
+                        if prev is not None:
+                            # Connect the new cell to its row
+                            cell.right, cell.left = prev.right, prev
+                            prev.right.left, prev.right = cell, cell
 
-                # Cover all satisfied columns
-                self.cover(cell.column)
-                row_cell = cell.right
-                while row_cell != cell:
-                    self.cover(row_cell.column)
-                    row_cell = row_cell.right
+                        prev = cell
+                else:
+                    # "z:n" has 2 items
+                    col_zone, col_num = column_name_items
+                    if get_zone(row, col) == col_zone and str(num) == col_num:
 
-        return solution_set
+                        cell = Cell(name=name)
+                        column.add_cell(cell)
+
+                        if prev is not None:
+                            # Connect the new cell to its row
+                            cell.right, cell.left = prev.right, prev
+                            prev.right.left, prev.right = cell, cell
+                        prev = cell
 
 if __name__ == '__main__':
 
